104fc323e1369e04dffdc50b4f8c1550f61ddddd
AleksaT2 <aleksandratatanova3830@gmail.com>
2024-04-06 15:29:14 +0300

    add hw

---
+++ src/js/Team.js
@@ -0,0 +1,37 @@
+import Character from "./Character";
+
+export default class Team {
+  constructor() {
+    this.players = [];
+    this.numberOfPlayers = 0;
+  }
+
+  add(player) {
+    if (player instanceof Character) {
+      this.players.push(player);
+      this.numberOfPlayers += 1;
+    } else {
+      throw new Error('Не валидный объект!');
+    }
+  }
+
+  [Symbol.iterator]() {
+    let current = 0;
+    const {players, numberOfPlayers} = this;
+    return {
+      next() {
+        if (current < numberOfPlayers) {
+          return {
+            value: players[current++],
+            done: false,
+          };
+        }
+        return {
+          value: undefined,
+          done: true,
+        };
+      },
+    };
+  }
+
+}
